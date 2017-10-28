b1c09ee9a2b80dee604d32901535399ed324befc
谢刘建 <377624289@qq.com>
2017-10-28 03:08:39 -0500

    调试client和server的protobuf通信

--- Server/src/server/msg/msg.go
+++ Server/src/server/msg/msg.go
@@ -1,11 +1,22 @@
 package msg
-
 import (
-	"github.com/name5566/leaf/network"
+	"github.com/name5566/leaf/network/protobuf"
 )
 
-var Processor network.Processor
+var (
+	Processor = protobuf.NewProcessor()
+)
 
-func init() {
+func init() {	// 这里我们注册了一个 protobuf 消息)
+    Processor.Register(&StartFight{})
+    Processor.Register(&FightResult{})
+    Processor.Register(&EnterFight{})
+    Processor.Register(&SignUpResponse{})
+    Processor.Register(&TosChat{})
+    Processor.Register(&TocChat{})
+    Processor.Register(&Login{})
+    Processor.Register(&PlayerBaseInfo{})
+    Processor.Register(&LoginSuccessfull{})
+    Processor.Register(&LoginFaild{})
 
 }
