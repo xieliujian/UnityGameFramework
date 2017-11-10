ab98668ad50b9182d868d6e856e3a0e94c57e161
谢刘建 <377624289@qq.com>
2017-11-10 08:02:39 -0600

    ios上通信调试

--- Client/Assets/Scripts/GameController.cs
+++ Client/Assets/Scripts/GameController.cs
@@ -7,12 +7,17 @@ public class GameController : MonoBehaviour
 {
     #region 内置函数
 
+	void Awake()
+	{
+		gameObject.AddComponent<NetManager>();
+
+		NetManager.Instance.SendConnect();
+	}
+
     // Use this for initialization
     void Start ()
     {
-        gameObject.AddComponent<NetManager>();
-
-        NetManager.Instance.SendConnect();
+        
 	}
 	
 	// Update is called once per frame
