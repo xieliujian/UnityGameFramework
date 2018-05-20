d0780ec57b368e6836e9d79e7b70d45b5bc09c3c
Xieliujian <377624289@qq.com>
2018-05-20 12:56:42 +0800

    增加资源管理模块

--- Client/Assets/Scripts/AppConst.cs
+++ Client/Assets/Scripts/AppConst.cs
@@ -6,6 +6,6 @@ public class AppConst
 {
     public static int SocketPort = 3563;                            //Socket服务器端口
 
-    //public static string SocketAddress = "127.0.0.1";                 //Socket服务器地址
-    public static string SocketAddress = "45.76.76.36";               //Socket服务器地址
+    public static string SocketAddress = "127.0.0.1";                 //Socket服务器地址
+    //public static string SocketAddress = "45.76.76.36";               //Socket服务器地址
 }
