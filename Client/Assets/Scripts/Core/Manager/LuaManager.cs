149e4d28249c7fa9477d455fdc7c21bdc92bcea8
Xieliujian <377624289@qq.com>
2018-05-29 00:19:12 +0800

    增加xlua

--- Client/Assets/Scripts/Core/Manager/LuaManager.cs
+++ Client/Assets/Scripts/Core/Manager/LuaManager.cs
@@ -2,11 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Util;
+using XLua;
 
 public class LuaManager : SingletonMonoBehaviour<MsgManager>
 {
-    void Start()
-    {
+    #region 变量
 
+    /// <summary>
+    /// lua环境
+    /// </summary>
+    private LuaEnv mLuaEnv = null;
+
+    #endregion
+
+    #region 内置函数
+
+    private void Start()
+    {
+        mLuaEnv = new LuaEnv();
     }
+
+    private void Update()
+    {
+        if (mLuaEnv != null)
+        {
+            mLuaEnv.Tick();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        mLuaEnv.Dispose();
+    }
+
+    #endregion
 }
