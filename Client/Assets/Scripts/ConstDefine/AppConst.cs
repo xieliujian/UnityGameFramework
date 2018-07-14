0a3a1e23397e72402173611c5dd285fd0640e4e6
Xieliujian <377624289@qq.com>
2018-07-14 18:49:52 +0800

    增加日志类

--- Client/Assets/Scripts/ConstDefine/AppConst.cs
+++ Client/Assets/Scripts/ConstDefine/AppConst.cs
@@ -10,8 +10,8 @@ public class AppConst
 {
     public const int SocketPort = 3563;                                             // Socket服务器端口
 
-    //public const string SocketAddress = "127.0.0.1";                              // Socket服务器地址
-    public static string SocketAddress = "45.76.76.36";                             // Socket服务器地址
+    public const string SocketAddress = "127.0.0.1";                              // Socket服务器地址
+    //public static string SocketAddress = "45.76.76.36";                             // Socket服务器地址
 
     public const string AppName = "UnityGameFramework";                             // 游戏名字
 
